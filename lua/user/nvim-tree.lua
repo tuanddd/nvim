ffa0afd62aa93d590ceec604b7e5b3a7d7507733
tuanddd <vincenttuandao@gmail.com>
2023-11-02 10:00:23 +0700

    fix: nvim-tree update focused file

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -16,5 +16,57 @@ local function open_nvim_tree(data)
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+vim.opt.termguicolors = true
 
-nvim_tree.setup()
+nvim_tree.setup({
+	view = {
+		width = 30,
+	},
+	reload_on_bufenter = true,
+	filters = {
+		exclude = { ".env", ".env.local" },
+	},
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	renderer = {
+		icons = {
+			glyphs = {
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
+		},
+		root_folder_modifier = ":~",
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+})
